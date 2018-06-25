88f47edcd39594fff44b8cce251336a0bfcb7ec8
Thomas Felix <thomas.felix@tfelix.de>
2018-06-26 00:41:31 +0200

    fix: Hopefully fixes prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -37,9 +37,9 @@ module.exports = {
       sourceMap: true
     }),
     new CopyWebpackPlugin([
-      'src/index.html',
-      'src/styles',
-      'assets'
+      { from: 'src/styles', to: 'styles'},
+      { from: 'src/index.html'},
+      { from: 'assets/**/*' }
     ])
   ]
 };
