0ea43b9acb8c2cb73e142f84a7bd1045006a4ffd
Thomas Felix <thomas.felix@tfelix.de>
2018-10-07 12:10:45 +0200

    Improves placing of phaser

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -78,6 +78,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-
-import 'phaser/dist/phaser';
