49c59a5a612a2c6012da3f9f35f1615b1eda8779
Thomas Felix <thomas.felix@tfelix.de>
2018-10-24 22:48:18 +0200

    Improves login screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,7 +5,7 @@ import { ModalService } from 'app/modal.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
