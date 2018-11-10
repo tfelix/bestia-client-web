ce4787e426fa9590ffec78a18d7cffe1f26c342b
Thomas Felix <thomas.felix@tfelix.de>
2018-11-10 16:54:37 +0100

    Corrects the dev entity placements

--- src/app/game/demo/RequestSyncHandler.ts
+++ src/app/game/demo/RequestSyncHandler.ts
@@ -30,18 +30,18 @@ export class RequestSyncHandler extends ClientMessageHandler<SyncRequestMessage>
     this.sendAllComponents(comps);
 
     // TODO Handle AI differently
-    const bestiaComps = this.entityFactory.addBestia('rabbit', new Point(5, 6));
+    const bestiaComps = this.entityFactory.addBestia('rabbit', new Point(16, 89));
     const entityId = bestiaComps[0].entityId;
     window.setTimeout(() => {
       const moveComp = new MoveComponent(23456, entityId);
       moveComp.path = [
-        new Point(4, 7),
-        new Point(4, 8),
-        new Point(4, 9),
-        new Point(3, 9),
-        new Point(2, 9),
-        new Point(3, 8),
-        new Point(4, 7)
+        new Point(16, 89),
+        new Point(16, 90),
+        new Point(16, 90),
+        new Point(15, 90),
+        new Point(16, 90),
+        new Point(16, 91),
+        new Point(16, 92)
       ];
       this.sendClient(new ComponentMessage<MoveComponent>(moveComp));
     }, 2000);
@@ -49,11 +49,11 @@ export class RequestSyncHandler extends ClientMessageHandler<SyncRequestMessage>
 
     // The creation of the server side entities should be done in a seperate component
     // fueld by the game demo scripts.
-    this.sendAllComponents(this.entityFactory.addBestia('rabbit', new Point(12, 12)));
+    this.sendAllComponents(this.entityFactory.addBestia('rabbit', new Point(23, 93)));
 
-    this.sendAllComponents(this.entityFactory.addObject('tree', new Point(10, 10)));
-    this.sendAllComponents(this.entityFactory.addObject('tree', new Point(14, 12)));
-    this.sendAllComponents(this.entityFactory.addObject('tree', new Point(18, 9)));
+    this.sendAllComponents(this.entityFactory.addObject('tree', new Point(31, 87)));
+    this.sendAllComponents(this.entityFactory.addObject('tree', new Point(30, 93)));
+    this.sendAllComponents(this.entityFactory.addObject('tree', new Point(13, 97)));
 
     this.sendAllComponents(this.entityFactory.addObject('tree_01', new Point(75, 95)));
 
@@ -65,21 +65,21 @@ export class RequestSyncHandler extends ClientMessageHandler<SyncRequestMessage>
     this.sendAllComponents([fxCompTree]);
     this.sendAllComponents(this.entityFactory.addDebugComponent(treeEntity));
 
-    this.sendAllComponents(this.entityFactory.addObject('plant', new Point(3, 4)));
-    this.sendAllComponents(this.entityFactory.addObject('plant', new Point(10, 8)));
-    this.sendAllComponents(this.entityFactory.addObject('plant', new Point(7, 8)));
+    this.sendAllComponents(this.entityFactory.addObject('plant', new Point(15, 89)));
+    this.sendAllComponents(this.entityFactory.addObject('plant', new Point(18, 90)));
+    this.sendAllComponents(this.entityFactory.addObject('plant', new Point(20, 88)));
 
     this.sendAllComponents(this.entityFactory.addObject('water', new Point(5, 8)));
 
-    this.sendAllComponents(this.entityFactory.addObject('sign', new Point(2, 8)));
+    this.sendAllComponents(this.entityFactory.addObject('sign', new Point(23, 85)));
     const signEntityId = this.entityFactory.getLastInsertedEntityId();
     const signEntity = this.serverEntities.getEntity(signEntityId);
     this.serverEntities.addIdentifier('sign_post', signEntity);
 
-    this.sendAllComponents(this.entityFactory.addItem('empty_bottle', 2, new Point(3, 12)));
-    this.sendAllComponents(this.entityFactory.addItem('empty_bottle', 1, new Point(7, 18)));
+    this.sendAllComponents(this.entityFactory.addItem('empty_bottle', 2, new Point(27, 94)));
+    this.sendAllComponents(this.entityFactory.addItem('empty_bottle', 1, new Point(31, 95)));
 
-    this.sendAllComponents(this.entityFactory.addItem('knife', 1, new Point(12, 10)));
-    this.sendAllComponents(this.entityFactory.addItem('knife', 1, new Point(3, 6)));
+    this.sendAllComponents(this.entityFactory.addItem('knife', 1, new Point(17, 93)));
+    this.sendAllComponents(this.entityFactory.addItem('knife', 1, new Point(26, 90)));
   }
 }
