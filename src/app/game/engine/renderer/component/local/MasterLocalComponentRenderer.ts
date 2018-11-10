e277b33c654d685c23f2a00ed5c0dd4e20e38931
Thomas Felix <thomas.felix@tfelix.de>
2018-11-10 15:34:19 +0100

    Master component renderer adds physics

--- src/app/game/engine/renderer/component/local/MasterLocalComponentRenderer.ts
+++ src/app/game/engine/renderer/component/local/MasterLocalComponentRenderer.ts
@@ -23,14 +23,17 @@ export class MasterLocalComponentRenderer extends ComponentRenderer<MasterLocalC
   }
 
   protected createGameData(entity: Entity, component: MasterLocalComponent) {
-    const sprite = entity.data.visual && entity.data.visual.sprite;
-    if (!sprite) {
+    const masterSprite = entity.data.visual && entity.data.visual.sprite;
+    if (!masterSprite) {
       return;
     }
 
     LOG.debug(`Setting camera follow to entity ${entity.id}.`);
-    this.ctx.game.cameras.main.startFollow(sprite);
+    this.ctx.game.cameras.main.startFollow(masterSprite);
     entityIdCameraFollows = entity.id;
+
+    // Enabling collision on the player sprite for event triggering of the engine.
+    this.ctx.game.physics.world.enable(masterSprite, Phaser.Physics.Arcade.DYNAMIC_BODY);
   }
 
   protected updateGameData(entity: Entity, component: MasterLocalComponent) {
