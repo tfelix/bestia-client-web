0b899c5cd94244fe9d5e5720a1328d152e2fa500
Thomas Felix <thomas.felix@tfelix.de>
2019-01-02 02:40:24 +0100

    Adds FTP deployment to server

---
+++ ci/ftp_deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+LOCALPATH='./dist/bestia-angular'
+REMOTEPATH='/demo.bestia-game.net'
+
+lftp -f "
+open ftp://${FTP_HOST}
+user ${FTP_USER} ${FTP_PASSWORD}
+mirror --continue --reverse --delete ${LOCALPATH} ${REMOTEPATH}
+bye
+"
