83c90892bf00719e8ad7d737aee2bc5f2f012064
Thomas Felix <thomas.felix@tfelix.de>
2019-01-02 04:39:56 +0100

    Ignores certificates

--- ci/ftp_deploy.sh
+++ ci/ftp_deploy.sh
@@ -4,9 +4,8 @@ set -e
 LOCALPATH='./dist/bestia-angular'
 REMOTEPATH='/demo.bestia-game.net'
 
-echo "set ssl:verify-certificate no" >> ~/.lftp/rc
-
 lftp -f "
+set ssl:verify-certificate no
 open ftp://${FTP_HOST}
 user ${FTP_USER} ${FTP_PASSWORD}
 mirror --continue --reverse --delete ${LOCALPATH} ${REMOTEPATH}
