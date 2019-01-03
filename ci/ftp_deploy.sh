26dbc0e1ba7dcf88e4d0c6fdc7b7b1e3945955fa
Thomas Felix <thomas.felix@tfelix.de>
2019-01-03 14:22:13 +0100

    Switches to SFTP deployment

--- ci/ftp_deploy.sh
+++ ci/ftp_deploy.sh
@@ -4,4 +4,5 @@ set -e
 LOCALPATH='./dist/bestia-angular'
 REMOTEPATH='demo.bestia-game.net'
 
-find ${LOCALPATH} -type f -exec curl -u ${FTP_USER}:${FTP_PASSWORD} --ftp-create-dirs -T {} ftp://bestia-game.net/${REMOTEPATH}/{} \;
+cd ${LOCALPATH}
+find . -type f -exec curl -u ${FTP_USER}:${FTP_PASSWORD} --ftp-create-dirs -T {} sftp://bestia-game.net/${REMOTEPATH}/{} \;
