5f85eb7c3b81f769dfe36530772974c80bb94e16
Thomas Felix <thomas.felix@tfelix.de>
2019-01-10 19:40:37 +0100

    Adds Heroku deployment

--- ci/ftp_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-LOCALPATH='./dist/bestia-angular'
-REMOTEPATH='demo.bestia-game.net'
-
-cd ${LOCALPATH}
-find . -type f -exec curl --ftp-ssl -u ${FTP_USER}:${FTP_PASSWORD} -k --ftp-create-dirs -T {} ftp://bestia-game.net/${REMOTEPATH}/{} \;
