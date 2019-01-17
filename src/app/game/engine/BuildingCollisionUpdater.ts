a7dc7c42bd1d8dc09dfb3d01db23829dce3edcb2
Thomas Felix <thomas.felix@tfelix.de>
2019-01-17 01:18:09 +0100

    Changes collision when inside the building
    Recognizes the building walls

--- src/app/game/engine/BuildingCollisionUpdater.ts
+++ src/app/game/engine/BuildingCollisionUpdater.ts
@@ -2,6 +2,7 @@ import { EngineContext } from './EngineContext';
 import { Entity, ComponentType, BuildingComponent, PositionComponent } from 'app/game/entities';
 import { BuildingDescription } from './renderer/component/BuildingComponentRenderer';
 import { CollisionMapUpdater, CollisionMap } from './CollisionMap';
+import { Vec2, Point } from '../model';
 
 export class BuildingCollisionUpdater implements CollisionMapUpdater {
 
@@ -22,7 +23,80 @@ export class BuildingCollisionUpdater implements CollisionMapUpdater {
   }
 
   private updateCollisionFromInsideBuilding(map: CollisionMap) {
+    const buildingEntityIds = this.ctx.collision.building.getCurrentOccupiedBuildingEntityIds();
 
+    if (buildingEntityIds.length === 0) {
+      return;
+    }
+
+    const firstBuildingId = buildingEntityIds[0];
+    const buildingDescName = (this.ctx.entityStore
+      .getComponentFromEntityId(firstBuildingId, ComponentType.BUILDING) as BuildingComponent)
+      .jsonDescriptionName;
+    const buildingDesc = this.ctx.gameScene.cache.json.get(buildingDescName) as BuildingDescription;
+
+    // Find all border tiles of the building.
+    const borderCandidates: Vec2[] = [];
+    const doors: Vec2[] = [];
+    buildingEntityIds.map(id => this.ctx.entityStore.getEntity(id))
+      .forEach(buildingEntity => {
+        const buildingComp = (buildingEntity.getComponent(ComponentType.BUILDING) as BuildingComponent);
+        const offset = (buildingEntity.getComponent(ComponentType.POSITION) as PositionComponent).position;
+        for (let dx = 0; dx < buildingDesc.blockSize; dx++) {
+          for (let dy = 0; dy < buildingDesc.blockSize; dy++) {
+
+            const p = new Point(dx + offset.x, dy + offset.y);
+            borderCandidates.push(p);
+
+            if (this.isDoor(dx, dy, buildingDesc, buildingComp)) {
+              doors.push(p);
+            }
+          }
+        }
+
+      });
+
+    const border = borderCandidates.map(bc => {
+      return { pos: bc, neighbours: this.countNeighbours(bc, borderCandidates) };
+    }).filter(bc => bc.neighbours !== 4)
+      // Remove Doors
+      .filter(bc => !this.isPointInArray(bc.pos, doors))
+      .map(bc => bc.pos);
+
+    const scrollOffset = this.ctx.helper.display.getScrollOffset();
+    border.forEach(b => {
+      map.setCollision(b.x - scrollOffset.x, b.y - scrollOffset.y, true);
+    });
+  }
+
+  /**
+   * Counts the direct neighbours of a tile.
+   */
+  private countNeighbours(tile: Vec2, tiles: Vec2[]): number {
+    const hasTop = tiles.findIndex(t => t.x === tile.x && t.y === tile.y - 1) !== -1;
+    const hasBottom = tiles.findIndex(t => t.x === tile.x && t.y === tile.y + 1) !== -1;
+    const hasLeft = tiles.findIndex(t => t.x === tile.x - 1 && t.y === tile.y) !== -1;
+    const hasRight = tiles.findIndex(t => t.x === tile.x + 1 && t.y === tile.y - 1) !== -1;
+
+    let neigbour = 0;
+    if (hasTop) {
+      neigbour++;
+    }
+    if (hasBottom) {
+      neigbour++;
+    }
+    if (hasLeft) {
+      neigbour++;
+    }
+    if (hasRight) {
+      neigbour++;
+    }
+
+    return neigbour;
+  }
+
+  private isPointInArray(p: Vec2, ps: Vec2[]): boolean {
+    return ps.findIndex(a => a.x === p.x && a.y === p.y) !== -1;
   }
 
   private updateCollisionWithBuildings(map: CollisionMap) {
@@ -67,11 +141,11 @@ export class BuildingCollisionUpdater implements CollisionMapUpdater {
     });
   }
 
-  private isDoor(x: number, y: number, desc: BuildingDescription, buildingComp: BuildingComponent): boolean {
+  private isDoor(dx: number, dy: number, desc: BuildingDescription, buildingComp: BuildingComponent): boolean {
     for (let i = 0; i < desc.doors.length; i++) {
       const d = desc.doors[i];
       if (d.name === buildingComp.innerSprite || d.name === buildingComp.outerSprite) {
-        if (d.position.x === x && d.position.y === y) {
+        if (d.position.x === dx && d.position.y === dy) {
           return true;
         }
       }
